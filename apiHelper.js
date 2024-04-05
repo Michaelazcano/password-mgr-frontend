d8238239e66ad6c3bcb892df9dbd194a139fa127
Michael <mikealazmacho@gmail.com>
2024-04-05 13:20:46 -0400

    save point

---
+++ apiHelper.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @param {string} consoleOut 
+ * sends stuff to console
+ */
+function test(consoleOut){
+    console.log(consoleOut)
+}
+
+function login(){
+
+}
+
+function logout(){
+
+}
+
+function addService(){
+
+}
+
+function addPassword(){
+
+}
+
+function getUsernames(){
+
+}
+
+function getPassword(){
+
+}
+storing jwt tokens 
