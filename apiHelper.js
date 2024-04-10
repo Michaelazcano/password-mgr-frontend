8b2965b2e3ab9a16a82ad11dda516a03924a9aa3
Michael <mikealazmacho@gmail.com>
2024-04-10 14:29:31 -0400

    Logic for login partially.

--- apiHelper.js
+++ apiHelper.js
@@ -1,14 +1,29 @@
 /**
  * 
- * @param {string} consoleOut 
+ * @param {"sends stuff to console"} consoleOut 
  * sends stuff to console
  */
 function test(consoleOut){
     console.log(consoleOut)
 }
-
-function login(){
-
+function login(Username,Password){
+    onLogin = fetch("http://127.0.0.1:5500/login",
+    {
+        method: "POST",
+        Headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "username":Username,
+            "password":Password
+        }).
+        then((request) => {
+            // document.cookie= ""
+            console.log(request)
+        })
+    
+    })
+   
 }
 
 function logout(){
@@ -30,4 +45,10 @@ function getUsernames(){
 function getPassword(){
 
 }
-storing jwt tokens 
+
+
+
+// storing jwt tokens Use an authentication
+//  gateway on the same domain, create an httponly cookie and
+//   set it to same-site. Then handle the JWTs in the 
+//   authentication gateway.
